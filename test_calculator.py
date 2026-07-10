e1b605ca1914bd84d7ff199baaaf2c5e4441bfd8
Aleflower01 <212617418+Aleflower01@users.noreply.github.com>
2026-07-10 11:37:19 +0200

    Aggiunge test mancante per la funzione sottrai

--- test_calculator.py
+++ test_calculator.py
@@ -1,8 +1,12 @@
-from calculator import somma, moltiplica
+from calculator import somma, sottrai, moltiplica
 
 
 def test_somma():
     assert somma(2, 3) == 5
+
+
+def test_sottrai():
+    assert sottrai(5, 3) == 2
 
 
 def test_moltiplica():
